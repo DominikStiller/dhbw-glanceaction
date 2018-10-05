7ef725811cce9ba7d340b5a592afa08215cabf64
Dominik Stiller <dominik.stiller@hpe.com>
2018-10-05 14:58:35 +0200

    FE: Add createCategory

--- frontend/src/app/services/backend.service.spec.ts
+++ frontend/src/app/services/backend.service.spec.ts
@@ -2,6 +2,7 @@ import { async, inject, TestBed } from '@angular/core/testing';
 
 import { BackendService } from './backend.service';
 import { HttpClientModule } from '@angular/common/http';
+import { Category } from '../models/category';
 
 describe('BackendService', () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -15,10 +16,24 @@ describe('BackendService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should issue a request', async(
-      inject([BackendService], (backend: BackendService) => {
-        backend.getCategories().subscribe(d => console.log(d));
-      }),
+  it('should get categories', async(
+    inject([BackendService], (backend: BackendService) => {
+      backend.getCategories().subscribe((data) => {
+        expect(data).toContain({ name: 'Food', color: '#ffffff' });
+      });
+    }),
+    ),
+  );
+  it('should create a category', async(
+    inject([BackendService], (backend: BackendService) => {
+      const c: Category = {
+        name: 'newcategory',
+        color: '#aabbcc',
+      };
+      backend.createCategory(c).subscribe((data) => {
+        expect(data).toEqual(c);
+      });
+    }),
     ),
   );
 });
--- frontend/src/app/services/backend.service.ts
+++ frontend/src/app/services/backend.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 
 import { environment } from '../../environments/environment';
 import { Category } from '../models/category';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+  }),
+};
 
 @Injectable({
   providedIn: 'root',
@@ -15,7 +23,30 @@ export class BackendService {
     return this.http.get<Category>(this.url('categories'));
   }
 
+  createCategory(category: Category) {
+    return this.http.post<Category>(this.url('categories'), category, httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
   private url(path: string) {
     return `${environment.apiBase}/${path}`;
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
   }
 }
