5482578b19d8473a372929a2eca29bbe12c2320c
Sascha <sascha.kipper@mailbox.org>
2018-10-07 16:29:36 +0200

    Erstes funktionierendes Backend
    Erste funktionierende Version des Backends, ohne besonders formatierten, aufgeräumten oder gegliederten Code.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
 const http = require('http');
-var util = require('util');
+const util = require('util');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(expressValidator());
+
 app.use(cookieParser());
 
 // Routes used for the different services
@@ -20,9 +24,9 @@ app.use(function (req, res, next) {
     next();
 });
 
-var server = http.createServer(app);
+let server = http.createServer(app);
 server.listen(4000, function () {
-    var port = server.address().port;
+    let port = server.address().port;
     util.log(util.format('Web Server listening at http://localhost:%s', port));
 });
 
--- backend/src/routes/accountRoutes.js
+++ backend/src/routes/accountRoutes.js
@@ -1,32 +1,133 @@
 const express = require('express');
-var db = require('../database').db;
-var util = require('util');
+const db = require('../database').db;
+const util = require('util');
+const { check, validationResult } = require('express-validator/check');
+const accounts = db.collection('Accounts');
+const transactions = db.collection('Transactions');
 
 const router = express.Router();
 
-// Accounts
+function changeIdOfAccount(account) {
+    let listAccount = account;
+    listAccount.id = listAccount._id;
+    delete listAccount._id;
+    return listAccount;
+}
+
+function handleInitialBalance(newAccount) {
+    let listAccount = newAccount;
+    listAccount.balance = listAccount.initialBalance;
+    delete listAccount.initialBalance;
+    return listAccount;
+}
+
+// REST Endpoints //
+
 router.get('/accounts', function(req, res){
-    var accounts = db.collection('Accounts');
     accounts.find().toArray(function (error, list) {
+        list.forEach(function (account, index, accArr) {
+            accArr[index] = changeIdOfAccount(account);
+        });
         res.json(list);
     });
 });
 
-router.post('/accounts', function(req, res){
+router.post('/accounts', [
+        check('name')
+            .not()
+            .isEmpty()
+            .withMessage('Name may not be empty')
+            .matches(/^[a-zA-Z].*/)
+            .withMessage('Name must start with a letter'),
+        check('initialBalance')
+            .not()
+            .isEmpty()
+            .withMessage('Initial balance may not be empty')
+            .isFloat()
+            .withMessage('Invalid initial balance')
+    ], function (req, res) {
+
     util.log(util.format('/api/accounts/ - POST - Request: %j', req.body));
 
-    var accounts = db.collection('Accounts');
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ error: {name: "AccountValidationError", message: errors.array()[0].msg} });
+    }
+
+    handleInitialBalance(req.body);
     accounts.insert(req.body, function (error, result) {
-        res.send('Successfully added');
+        accounts.findOne(result, function (error, result) {
+            res.status(201).send(changeIdOfAccount(result));
+        });
     });
 });
 
-router.put('/accounts/:id', function(req, res){
-    res.send({type:'PUT'});
+router.put('/accounts/:id([0-9]+)', [
+        check('name')
+            .not()
+            .isEmpty()
+            .withMessage('Name may not be empty')
+            .matches(/^[a-zA-Z].*/)
+            .withMessage('Name must start with a letter'),
+        check('initialBalance')
+            .not()
+            .isEmpty()
+            .withMessage('Initial balance may not be empty')
+            .isFloat()
+            .withMessage('Invalid initial balance')
+    ], function (req, res) {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ error: {name: "AccountValidationError", message: errors.array()[0].msg} });
+    }
+
+    let newName = req.body.name;
+    let newBalance = req.body.initialBalance;
+    let accountId = req.params.id;
+
+    accounts.findOne({_id:accountId}, function(err, account){
+        if(err) {
+            return res.status(500).send();
+        }
+        if(!Boolean(account)) {
+            return res.status(404).json({ error: {name: "AccountUndefinedError", message: "The account to be changed doesn't exist"}});
+        } else {
+            accounts.update({_id: accountId}, {$set: {name: newName, balance: newBalance}}, function (err, account) {
+                if (err) {
+                    return res.status(500).send();
+                }
+                res.status(204).send("Success");
+            });
+        }
+    });
+
 });
 
-router.delete('/accounts/:id', function(req, res){
-    res.send({type:'DELETE'});
+router.delete('/accounts/:id([0-9]+)', function(req, res){
+    let accountId = req.params.id;
+
+    // Deleting an account also deleted all transactions associated with this account. User confirmation in frontend!
+    accounts.findOne({_id:accountId}, function(err, account){
+        if(err) {
+            return res.status(500).send();
+        }
+        if(!Boolean(account)) {
+            return res.status(404).json({ error: {name: "AccountUndefinedError", message: "The account to be deleted doesn't exist"}});
+        } else {
+            transactions.remove({account: accountId}, function(err, resTransactions) {
+                if (err) {
+                    return res.status(500).send();
+                }
+                accounts.findAndRemove({_id:accountId}, function(err, accountDel){
+                    if (err) {
+                        return res.status(500).send();
+                    }
+                    res.status(204).send("Success");
+                });
+            });
+        }
+    });
 });
 
 module.exports = router;
