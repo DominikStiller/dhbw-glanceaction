c018c70bf5388f36eb2871759468e80a0b9140f4
Dominik Stiller <domi.stiller@gmail.com>
2018-10-13 23:18:38 +0200

    FE: Add e2e test for transaction creation and deletion

--- frontend/e2e/src/app.e2e-spec.ts
+++ frontend/e2e/src/app.e2e-spec.ts
@@ -13,9 +13,30 @@ describe('GlanceAction', () => {
     expect(page.getTransactions().count()).toBeGreaterThan(0);
   });
 
-  it('should show a higher ending balance when adding a positive transaction', () => {
+  it('should show a higher ending balance when adding a positive transaction, deleting it afterwards', async () => {
+    // Part 1: Test creation
+    const balanceToFloat = balanceString => Number(balanceString
+        .substring(1)
+        .replace(',', ''));
+    const amountToAdd = 2646.15;
+    const initialBalance = balanceToFloat(await page.getEndingBalance());
+
+    // Create transaction
     page.getCreateTransactionButton().click();
-    browser.getCurrentUrl()
-      .then(url => expect(url.endsWith('/create')).toBeTruthy());
+    page.getAmountInput().sendKeys(amountToAdd);
+    page.getNotesInput().sendKeys('Created by Protractor');
+    page.getCreateSaveTransactionButton().click();
+
+    expect(balanceToFloat(await page.getEndingBalance())).toBeCloseTo(initialBalance + amountToAdd, 2);
+
+    // Part 2: Test deletion
+    const createdByProtractor = page.getTransactionsCreatedByProtractor();
+    const count = await createdByProtractor.count();
+
+    // Delete transaction
+    createdByProtractor.first().click();
+    page.getDeleteTransactionButton().click();
+
+    expect(page.getTransactionsCreatedByProtractor().count()).toEqual(count - 1);
   });
 });
--- frontend/e2e/src/app.po.ts
+++ frontend/e2e/src/app.po.ts
@@ -6,7 +6,8 @@ export class AppPage {
   }
 
   getTransactions() {
-    return element(by.tagName('app-transaction-list')).all(by.css('.transaction'));
+    return element(by.tagName('app-transaction-list'))
+      .all(by.css('.transaction'));
   }
 
   getCreateTransactionButton() {
@@ -15,5 +16,31 @@ export class AppPage {
       .last()
       .all(by.tagName('button'))
       .last();
+  }
+
+  getEndingBalance() {
+    return element(by.css('#ending-balance')).getText();
+  }
+
+  getAmountInput() {
+    return element(by.css('#input-amount'));
+  }
+
+  getNotesInput() {
+    return element(by.css('#input-notes'));
+  }
+
+  getCreateSaveTransactionButton() {
+    return element(by.tagName('app-create-edit-transaction'))
+      .element(by.buttonText('Create'));
+  }
+
+  getDeleteTransactionButton() {
+    return element(by.tagName('app-create-edit-transaction'))
+      .element(by.partialButtonText('Delete'));
+  }
+
+  getTransactionsCreatedByProtractor() {
+    return element.all(by.cssContainingText('app-transaction-list .transaction', 'Created by Protractor'));
   }
 }
