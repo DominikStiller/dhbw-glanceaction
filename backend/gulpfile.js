3384493c9dbfade607f916007af781a3952731e5
Sascha <sascha.kipper@mailbox.org>
2018-10-14 10:07:25 +0200

    BE: Added JSDoc and more Gulp tasks
    BE: Added more Gulp tasks containing the generation of a JSDoc3 documentation; although there is not much to document in the backend.

--- backend/gulpfile.js
+++ backend/gulpfile.js
@@ -2,10 +2,16 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 const uglify = require('gulp-uglify-es').default;
+const clean = require('gulp-clean');
+const jsdoc = require('gulp-jsdoc3');
+const config = require('./.jsdoc.json');
 
 const files = {
   projectSources: [
     'src/**/*.js',
+  ],
+  buildFiles: [
+    'build/**/*.js',
   ],
 };
 
@@ -19,5 +25,15 @@ gulp.task('lintSources', () => {
 gulp.task('minify', () => {
   return gulp.src(files.projectSources)
     .pipe(uglify())
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('clean', () => {
+  return gulp.src('./build', { read: false })
+    .pipe(clean());
+});
+
+gulp.task('generateDocumentation', (cb) => {
+  gulp.src(files.projectSources, { read: false })
+    .pipe(jsdoc(config, cb));
 });
